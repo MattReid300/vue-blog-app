429fccb1d2ffd1109a7adcb116515a19b5f4510e
MattReid <matt.reid300@gmail.com>
2021-07-05 13:27:33 +0200

    Pagination update

--- src/views/ArticleShow.vue
+++ src/views/ArticleShow.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="main">
+    <div class="buttons">
+      <button class="btn-left" v-if="id > 1" @click="prevArticle">
+        <b-icon
+          icon="arrow-left-square-fill"
+          scale="1.3"
+          style="margin-right: 10px"
+        ></b-icon>
+        Previous
+      </button>
+      <button
+        class="btn-right"
+        v-if="id < $store.getters.totalArticles"
+        @click="nextArticle"
+      >
+        Next
+        <b-icon
+          icon="arrow-right-square-fill"
+          scale="1.3"
+          style="margin-left: 10px"
+        ></b-icon>
+      </button>
+    </div>
     <h3>
       <b
         ><i>{{ article.title }}</i></b
@@ -13,20 +35,41 @@
 
 <script>
 import APIService from "@/APIService.js";
+import { mapState } from "vuex";
 export default {
   props: ["id"], // "id" has been passed as a prop when I import this file to its parent
   data() {
     return {
-      article: {},
+      article: {}
     };
   },
-  created() {
-    APIService.getArticle(this.id).then((response) => {
-      this.article = response.data;
-    });
-    // .catch(error => {
-    //   console.log("There was an error:", error.response);
-    // });
+  computed: {
+    ...mapState(["articles"])
+  },
+  mounted() {
+    this.fetchData();
+    if (this.articles == 0) {
+      this.$store.dispatch("fetchArticles");
+    }
+  },
+  methods: {
+    fetchData(id = this.id) {
+      APIService.getArticle(id).then(response => {
+        this.article = response.data;
+      });
+    },
+    nextArticle() {
+      const current = this.$route.params.id;
+      const newId = Number(current) + 1;
+      this.$router.push(`/articles/${newId}`);
+      this.fetchData(newId);
+    },
+    prevArticle() {
+      const current = this.$route.params.id;
+      const newId = Number(current) - 1;
+      this.$router.push(`/articles/${newId}`);
+      this.fetchData(newId);
+    }
   }
 };
 </script>
@@ -35,5 +78,17 @@ export default {
 .main {
   text-align: left;
   margin-left: 50px;
+}
+.buttons {
+  margin: 30px auto;
+  display: flex;
+}
+.btn-left {
+  display: block;
+  margin-right: auto;
+}
+.btn-right {
+  display: block;
+  margin-left: auto;
 }
 </style>
