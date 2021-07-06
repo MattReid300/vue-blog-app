96b55121010cc094882e813c16996ecfd82c0b07
MattReid300 <61089040+MattReid300@users.noreply.github.com>
2021-07-06 17:02:52 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    showAddArticleLink: false,
+    showAddArticleLink: true,
     adminPassword: "letmepass",
     showDeleteButton: false,
     articles: []
