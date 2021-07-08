126bc81642287053da85551c054717422b18ad02
MattReid <matt.reid300@gmail.com>
2021-07-08 10:34:34 +0200

    Router bug fix

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import ArticleAdder from "../components/ArticleAdder";
 import ArticleShow from "../views/ArticleShow.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -18,8 +19,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
     path: "/articleadder",
